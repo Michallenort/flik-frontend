31eb29bd90f952573ad44192d0d941b319919d20
Michallenort <michal.lenort@gmail.com>
2025-01-21 23:35:11 +0100

    Project setup

---
+++ constants/icons.js
@@ -0,0 +1,29 @@
+import bookmark from "../../../../Downloads/assets/icons/bookmark.png";
+import home from "../../../../Downloads/assets/icons/home.png";
+import plus from "../../../../Downloads/assets/icons/plus.png";
+import profile from "../../../../Downloads/assets/icons/profile.png";
+import leftArrow from "../../../../Downloads/assets/icons/left-arrow.png";
+import menu from "../../../../Downloads/assets/icons/menu.png";
+import search from "../../../../Downloads/assets/icons/search.png";
+import upload from "../../../../Downloads/assets/icons/upload.png";
+import rightArrow from "../../../../Downloads/assets/icons/right-arrow.png";
+import logout from "../../../../Downloads/assets/icons/logout.png";
+import eyeHide from "../../../../Downloads/assets/icons/eye-hide.png";
+import eye from "../../../../Downloads/assets/icons/eye.png";
+import play from "../../../../Downloads/assets/icons/play.png";
+
+export default {
+  play,
+  bookmark,
+  home,
+  plus,
+  profile,
+  leftArrow,
+  menu,
+  search,
+  upload,
+  rightArrow,
+  logout,
+  eyeHide,
+  eye,
+};
