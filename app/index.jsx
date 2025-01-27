7bcb03f9a56652ace55e3323e02aac25c98bb1a4
Michallenort <michal.lenort@gmail.com>
2025-01-27 23:29:29 +0100

    Added OTP authorization

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,14 @@
-import { Link } from "expo-router";
+import { Link, useRouter, Redirect } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, {useEffect} from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-    return (
-        <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-3xl font-pbold">Open</Text>
-            <StatusBar style="auto" />
+    //const router = useRouter();
 
-        </View>
-    );
+    // useEffect(() => {
+    //     router.replace("/login")
+    // }, []);
+
+    return <Redirect href="/login" />;
 }
